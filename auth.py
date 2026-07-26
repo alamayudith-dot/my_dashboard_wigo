72d687cf969e6c2955edf3625adb2bd6702e4c94
alamayudith-dot <alamayudith@gmail.com>
2026-07-25 22:54:18 -0500

    Add login function for user authentication
    Implement login functionality with user authentication.

---
+++ auth.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+USUARIO = "Infouni"
+PASSWORD = "12345"
+
+def login():
+
+    st.title("🔐 WIGO MOTORS S.A.C.")
+    st.subheader("Inicio de sesión")
+
+    usuario = st.text_input("Usuario")
+    password = st.text_input("Contraseña", type="password")
+
+    if st.button("Ingresar"):
+
+        if usuario == USUARIO and password == PASSWORD:
+
+            st.session_state["login"] = True
+            st.session_state["usuario"] = usuario
+            st.rerun()
+
+        else:
+            st.error("Usuario o contraseña incorrectos")
